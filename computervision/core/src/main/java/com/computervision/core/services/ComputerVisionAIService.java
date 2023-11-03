fe1497accc99030cffefbce147129470ee67ad06
LL\prikulka <prikulka@publicisgroupe.net>
2023-11-03 15:46:56 +0530

    initial commit for ai computer image analyser

---
+++ computervision/core/src/main/java/com/computervision/core/services/ComputerVisionAIService.java
@@ -0,0 +1,9 @@
+package com.computervision.core.services;
+
+/**
+ * @author Priti_Kulkarni
+ *
+ */
+public interface ComputerVisionAIService {
+  public String getMetaData(String imageLocation);
+}
